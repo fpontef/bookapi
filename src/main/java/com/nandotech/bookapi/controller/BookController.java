ceb7a06a85af14b76e1727780b345f8d814134f3
fpontef <sobralense@gmail.com>
2021-12-11 21:34:26 -0300

    refactor @AutoWired annotation & remove unecessary Constructor using Lombok

--- src/main/java/com/nandotech/bookapi/controller/BookController.java
+++ src/main/java/com/nandotech/bookapi/controller/BookController.java
@@ -4,6 +4,7 @@ import com.nandotech.bookapi.dto.request.BookDTO;
 import com.nandotech.bookapi.dto.response.MessageResponseDTO;
 import com.nandotech.bookapi.service.BookService;
 import com.nandotech.bookapi.service.exception.BookNotFoundException;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/book")
+@NoArgsConstructor(onConstructor = @__(@Autowired))
 public class BookController {
 
     private BookService bookService;
-
-    @Autowired
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
 
     // Annotation @RequestBody do the same as nodejs express req.body() to accept key-value paris
     @PostMapping // HTTP VERB POST
